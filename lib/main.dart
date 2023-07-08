2e699b70ccf0827326bf46115d82b08fafcdff60
XxDEAMxX <deam10hw@gmail.com>
2023-07-08 17:50:00 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader/screens/screens.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'QR Reader',
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => HomeScreen(),
+        'mapa': (context) => MapScreen()
+      },
+    );
+  }
+}
